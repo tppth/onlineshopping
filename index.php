7546e76ccda6a6695f68f50068a628bb08833a69
tppth <tpplearning@gmail.com>
2020-02-05 16:14:54 +0630

    'first-commit'

---
+++ index.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<script type="text/javascript" src="bootstrap/js/jquery.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="fontawesome/css/all.min.css">
+	<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="bootstrap/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+		<a class="navbar-brand" href="#" style="text-transform: uppercase; font-weight: bold;">Buy More</a>
+		<a class="navbar-brand d-lg-block d-md-none" href="#">
+			<nav class="navbar navbar-light bg-light">
+				<form class="form-inline">
+					<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+					<button class="btn btn-primary my-2 my-sm-0" type="submit">Search</button>
+				</form>
+			</nav>
+		</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarNav">
+			<ul class="navbar-nav ml-auto">
+				<li class="nav-item active">
+					<a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#"><i class="fas fa-shopping-bag"></i></a>
+				</li>
+			</ul>
+		</div>
+	</nav>
+
+	<div class="carousel slide" data-ride="carousel">
+		<div class="carousel-inner">
+			<div class="carousel-item active">
+				<img class="d-block w-100" src="images/banner_1.jpg" alt="First slide">
+			</div>
+		</div>
+	</div>	
+
+	<div style="height: auto;">
+		<div class="container font-weight-bold">
+			<h3 class="py-5">Acer Gaming Laptops</h3>
+			<div class="row text-center">
+				<div class="col-lg-3 col-md-6 col-sm-12 pb-3">
+					<img src="images/acer_2.jpeg" class="img-fluid">
+					<p class="card-title">Acer Nitro 5</p>
+					<p class="card-title">$699,900</p>
+					<button class="btn btn-primary">Add to cart</button>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-12 pb-3">
+					<img src="images/acer_2.jpeg" class="img-fluid">
+					<p class="card-title">Acer Helios 300</p>
+					<p class="card-title">$1,699,900</p>
+					<button class="btn btn-primary">Add to cart</button>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-12 pb-3">
+					<img src="images/acer_3.jpeg" class="img-fluid">
+					<p class="card-title">Acer Nitro 5</p>
+					<p class="card-title">$629,900</p>
+					<button class="btn btn-primary">Add to cart</button>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-12 pb-3">
+					<img src="images/acer_3.jpeg" class="img-fluid">
+					<p class="card-title">Acer Nitro 5</p>
+					<p class="card-title">$679,900</p>
+					<button class="btn btn-primary">Add to cart</button>
+				</div>
+			</div>
+			<br>
+			<h3 class="py-5">Asus Gaming Laptops</h3>
+			<div class="row text-center">
+				<div class="col-lg-3 col-md-6 col-sm-12 pb-3">
+					<img src="images/asus_2.jpeg" class="img-fluid">
+					<p class="card-title">Asus ROG Zephryus G</p>
+					<p class="card-title">$799,900</p>
+					<button class="btn btn-primary">Add to cart</button>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-12 pb-3">
+					<img src="images/asus_2.jpeg" class="img-fluid">
+					<p class="card-title">Asus ROG Strix G</p>
+					<p class="card-title">$899,900</p>
+					<button class="btn btn-primary">Add to cart</button>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-12 pb-3">
+					<img src="images/asus_3.jpeg" class="img-fluid">
+					<p class="card-title">Asus ROG Strix G</p>
+					<p class="card-title">$679,900</p>
+					<button class="btn btn-primary">Add to cart</button>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-12 pb-3">
+					<img src="images/asus_3.jpeg" class="img-fluid">
+					<p class="card-title">Asus ROG Zephryus S</p>
+					<p class="card-title">$2,399,900</p>
+					<button class="btn btn-primary">Add to cart</button>
+				</div>
+			</div>
+			<br>
+			<h3 class="py-5">MSI Gaming Laptops</h3>
+			<div class="row text-center">
+				<div class="col-lg-3 col-md-6 col-sm-12 pb-3">
+					<img src="images/msi_2.jpeg" class="img-fluid">
+					<p class="card-title">MSI GL Series</p>
+					<p class="card-title">$609,900</p>
+					<button class="btn btn-primary">Add to cart</button>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-12 pb-3">
+					<img src="images/msi_2.jpeg" class="img-fluid">
+					<p class="card-title">MSI GL</p>
+					<p class="card-title">$999,900</p>
+					<button class="btn btn-primary">Add to cart</button>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-12 pb-3">
+					<img src="images/msi_2.jpeg" class="img-fluid">
+					<p class="card-title">MSI G</p>
+					<p class="card-title">$749,900</p>
+					<button class="btn btn-primary">Add to cart</button>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-12 pb-3">
+					<img src="images/msi_2.jpeg" class="img-fluid">
+					<p class="card-title">MSI Stealth</p>
+					<p class="card-title">$1,899,900</p>
+					<button class="btn btn-primary">Add to cart</button>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div style="bottom: 0;" class="bg-light">
+		<div class="footer-copyright text-center py-3">© 2020 Copyright:
+			<a href="https://mdbootstrap.com/education/bootstrap/"> BUYMORE.COM</a>
+		</div>
+	</div>
+</body>
+</html>
+
